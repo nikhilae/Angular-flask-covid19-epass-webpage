088b8ebed00c6bc372252a9321c9e2014a53df76
Nikhila <etnnikhi@gmail.com>
2021-05-22 22:19:57 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CountriesComponent } from './components/countries/countries.component';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
+
 
 const routes: Routes = [
-  {path: '',component:HomeComponent},
-  {path: 'countries', component: CountriesComponent}
-
+  {path : '' , component : HomeComponent},
+  {path : 'countries' , component :CountriesComponent } 
+  
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
 import { GlobalDataSummary } from 'src/app/models/global-data';
-import { DataServicesService } from 'src/app/services/data-service.service';
 
 @Component({
   selector: 'app-home',
@@ -8,32 +8,98 @@ import { DataServicesService } from 'src/app/services/data-service.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  totalConfirmed=0;
-  totalActive=0;
-  totalDeaths=0;
-  totalRecovered=0;
-  globalData:GlobalDataSummary[];
-  constructor(private dataService:DataServicesService) { }
 
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  loading = true;
+  globalData: GlobalDataSummary[];
+  datatable = [];
+  chart = {
+    PieChart : "PieChart" ,
+    ColumnChart : 'ColumnChart' ,
+    LineChart : "LineChart", 
+    height: 500, 
+    options: {
+      animation:{
+        duration: 1000,
+        easing: 'out',
+      },
+      is3D: true
+    }  
+  }
+  
+  
+  constructor(private dataService: DataServiceService) { }
+
+
+  
   ngOnInit(): void {
+
     
     this.dataService.getGlobalData()
-    .subscribe(
-      {
-        next:(result)=>{
-          console.log(result)
-          this.globalData=result;
-          result.forEach(cs=>{
-            if(!Number.isNaN(cs.confirmed)){
-            this.totalConfirmed+=cs.confirmed
-            this.totalActive+=cs.active
-            this.totalDeaths+=cs.deaths
-            this.totalRecovered+=cs.recovered
-            }
-          })
+      .subscribe(
+        {
+          next: (result) => {
+            console.log(result);
+            this.globalData = result;
+            result.forEach(cs => {
+              if (!Number.isNaN(cs.confirmed)) {
+                this.totalActive += cs.active
+                this.totalConfirmed += cs.confirmed
+                this.totalDeaths += cs.deaths
+                this.totalRecovered += cs.active
+              }
+
+            })
+
+            this.initChart('c');
+          }, 
+          complete : ()=>{
+            this.loading = false;
+          }
         }
-      }
-    )
+      )
   }
+
+
+
+  updateChart(input: HTMLInputElement) {
+    console.log(input.value);
+    this.initChart(input.value)
+  }
+
+  initChart(caseType: string) {
+
+    this.datatable = [];
+    // this.datatable.push(["Country", "Cases"])
+    
+    this.globalData.forEach(cs => {
+      let value :number ;
+      if (caseType == 'c')
+        if (cs.confirmed > 2000)
+          value = cs.confirmed
+          
+      if (caseType == 'a')
+        if (cs.active > 2000)
+          value = cs.active
+      if (caseType == 'd')
+        if (cs.deaths > 1000)
+          value = cs.deaths
+          
+      if (caseType == 'r')
+        if (cs.recovered > 2000)
+            value = cs.recovered
+        
+
+        this.datatable.push([
+            cs.country, value
+          ])
+    })
+    console.log(this.datatable);
+
+  }
+  
 
 }
